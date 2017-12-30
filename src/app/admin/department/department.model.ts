d9029b89d2e03dd6c4293d042dda293fca8ded29
to-gaurav <gaurav.usb@gmail.com>
2017-12-30 15:37:33 -0700

    ui

--- src/app/admin/department/department.model.ts
+++ src/app/admin/department/department.model.ts
@@ -3,14 +3,14 @@ export class Department {
     public departmentName: string;
     public departmentHeadId: number;
     public obsolete: string;
-    public entity_id: number;
+    public entityId: number;
+    public schoolYearId: number;
     public departmentParticulars: Array<DepartmentParticulars>;
 }
 
 export class DepartmentParticulars {
     public subjectId: string;
     public departmentId: string;
-    //public recordId: string;
     public subjectName: string;
     public subSubjectName: string;
     public obsolete: string;
